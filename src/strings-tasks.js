ef676e520f388cfdc53fdf06e7e6536b4092abb1
yanasaa <yanasaa@gmail.com>
2023-12-17 13:30:31 +0300

    fix: reverseWords func

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -353,8 +353,11 @@ function findLongestWord(/* sentence */) {
  *   reverseWords('Hello World') => 'olleH dlroW'
  *   reverseWords('The Quick Brown Fox') => 'ehT kciuQ nworB xoF'
  */
-function reverseWords(/* str */) {
-  throw new Error('Not implemented');
+function reverseWords(str) {
+  return str
+    .split(' ')
+    .map((word) => word.split('').reverse().join(''))
+    .join(' ');
 }
 
 /**
